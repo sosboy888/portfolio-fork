94cc20ab16e99fc0dcc824f08cc9f1a4d468801b
Larkin Campbell <Larkincampbell24@gmail.com>
2024-08-06 16:14:03 -0700

    editing bentoGrid component

--- app/components/Grid.tsx
+++ app/components/Grid.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { BentoGrid } from './ui/BentoGrid'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 import { title } from 'process'
 
 const Grid = () => {
   return (
     <section id='about'>
         <BentoGrid>
-            {[{ title: 'Title1', description: 'Desc1'}]}d
+            {[{ title: 'Title1', description: 'Desc1', id: 1}].map((item) => (
+                <BentoGridItem 
+                id={item.id}
+                key={item.id}
+                title={item.title}
+                description={item.description}
+                />
+            ))}
         </BentoGrid>
     </section>
   )

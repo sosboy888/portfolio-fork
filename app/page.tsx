084903cf0a63bc31d55d324904eebc69f80f8df3
Larkin Campbell <Larkincampbell24@gmail.com>
2024-08-11 16:27:40 -0700

    itssss broken

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import { FloatingNav } from "./components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import Grid from "./components/Grid";
+import RecentsProjects from "./components/RecentsProjects";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
           ]}/>
           <Hero />
           <Grid />
+          <RecentsProjects />
         </div>
       </main>
   );

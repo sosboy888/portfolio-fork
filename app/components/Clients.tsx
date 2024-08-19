5ba79f8bf72a3f6fe0e6a6b8f18b5337d61a14fa
Larkin Campbell <Larkincampbell24@gmail.com>
2024-08-18 22:45:29 -0700

    concluding clients section

--- app/components/Clients.tsx
+++ app/components/Clients.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
-import { testimonials } from '@/data'
+import { companies, testimonials } from '@/data'
+import { div } from 'three/webgpu'
 
 const Clients = () => {
   return (
@@ -9,14 +10,33 @@ const Clients = () => {
         Kind words from {' '}
         <span className='text-purple'>Enamored coworkers</span>
     </h1>
-        <div className='flex flex-col items-center'>
-            <div className='h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden'>
+        <div className='flex flex-col items-center max-lg:mt-10 '>
             <InfiniteMovingCards 
                 items={testimonials}
                 direction='right'
                 speed='slow'
                 />
-            </div>
+             <h1 className='heading'>
+        Companies I derive {' '}
+        <span className='text-purple'>inspiration</span>
+    </h1>
+        <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt10'>
+            {companies.map(({id, img, name, nameImg}) => (
+                <div key={id} className='flex md:max-w-60 max-w-32 gap-2'>
+                    <img 
+                        src={img}
+                        alt={name}
+                        className='md:w-24 w-20'
+                    />
+                    <img 
+                        src={nameImg || null}
+                        alt={name}
+                        className='md:w-24 w-20'
+                    />
+                </div>
+
+            ))}
+        </div>
         </div>
     </div>
   )
--- app/components/ui/InfiniteMovingCards.tsx
+++ app/components/ui/InfiniteMovingCards.tsx
@@ -73,7 +73,7 @@ export const InfiniteMovingCards = ({
     <div
       ref={containerRef}
       className={cn(
-        "scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        "scroller relative z-20  w-screen overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
@@ -100,22 +100,24 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 leading-[1.6] text-white text-lg font-normal">
+              <span className=" relative z-20 leading-[1.6] text-white text-sm md:text-lg font-normal">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
                     <div className="me-3 ">
-                        <img src="/profile.svg" alt="profiel"/>
+                        <img src="/Profile.svg" alt="profile"/>
                     </div>
+                    <div className="flex flex-col gap-1">
                   <span className=" text-xl leading-[1.6] text-white font-bold">
                     {item.name}
                   </span>
                   <span className=" text-sm leading-[1.6] text-white-200 font-normal">
                     {item.title}
                   </span>
-                </span>
-              </div>
+                  </div>
+                  </span>
+                </div>
             </blockquote>
           </li>
         ))}

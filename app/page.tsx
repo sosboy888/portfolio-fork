7869391423db27be6bee9caa53cf6aa6ce94a9dc
Larkin Campbell <Larkincampbell24@gmail.com>
2024-08-26 22:18:11 -0700

    Canvas Reveal Effect

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import RecentsProjects from "./components/RecentsProjects";
 import Clients from "./components/Clients";
 import Experience from "./components/Experience";
 import { FaComputer, FaPerson } from "react-icons/fa6";
+import Approach from "./components/Approach";
 
 export default function Home() {
   return (
@@ -15,16 +16,17 @@ export default function Home() {
         <div className="max-w-7xl w-full">
           <FloatingNav 
           navItems={[
-            {name: 'About', link: '/', icon: <FaHome />},
-            {name: 'Projects', link: '/projects', icon: <FaComputer /> },
-            {name: 'Testimonials', link: '/testimonials', icon: <FaPerson />},
-            {name: 'Contact', link: '/contact'}
+            {name: 'About', link: '#about', icon: <FaHome />},
+            {name: 'Projects', link: '#projects', icon: <FaComputer /> },
+            {name: 'Testimonials', link: '#testimonials', icon: <FaPerson />},
+            {name: 'Contact', link: '#contact'}
           ]}/>
           <Hero />
           <Grid />
           <RecentsProjects />
           <Clients />
           <Experience />
+          <Approach />
         </div>
       </main>
   );
